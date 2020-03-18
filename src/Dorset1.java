92bbeb90c53a179cee5cbb4ece73ffaced3f8c33
GKemper1989 <g.kemperweber@gmail.com>
2020-03-18 16:25:38 +0000

    problem 1

--- src/Dorset1.java
+++ src/Dorset1.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Dorset1 {
 
-    public static void main (String[] args)
-    {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int sum = 0;
         System.out.println("Enter a 4 digits number: ");
@@ -13,8 +12,7 @@ public class Dorset1 {
 
         char[] ArrayNumber = new char[string.length()];
 
-        for (int i = 0; i < string.length(); i++)
-        {
+        for (int i = 0; i < string.length(); i++) {
             ArrayNumber[i] = string.charAt(i);
             int result = Integer.parseInt((String.valueOf(ArrayNumber[i])));
             sum += result;
